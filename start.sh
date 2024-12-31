e61bafeddbfd6de276cd2ded4240de8799d35b5c
amang06 <aman@skynetiks.com>
2024-12-31 13:23:32 +0530

    remove start script

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the Go binary
-echo "Building the Go binary..."
-go build -o skyfunnel-email-verifier-api
-
-# Run the built binary
-echo "Starting the application..."
-./skyfunnel-email-verifier-api
